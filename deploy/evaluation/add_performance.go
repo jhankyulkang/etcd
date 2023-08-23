5b4caef8f76ce7c23dad7d2e01f218a9b4829b7d
jhankyulkang <jhankyulkang@gmail.com>
2023-08-23 13:14:59 -0400

    ssh added to experiment - testing

--- deploy/evaluation/add_performance.go
+++ deploy/evaluation/add_performance.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
-	"fmt"
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
+
+	//"encoding/json"
+	"fmt"
+	"github.com/melbahja/goph"
+	//"go.etcd.io/etcd/api/v3/etcdserverpb"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+	"io"
+	"os"
 	//v3 "go.etcd.io/etcd/client/v3"
 	"log"
 	//"os"
@@ -12,6 +21,15 @@ import (
 	"time"
 )
 
+type Config struct {
+	Auth           goph.Auth
+	User           string
+	Addr           string
+	Port           uint
+	Timeout        time.Duration
+	Callback       ssh.HostKeyCallback
+	BannerCallback ssh.BannerCallback
+}
 type addReport struct {
 	Start int64 `json:"start"`
 	Issue int64 `json:"issue"`
@@ -31,7 +49,7 @@ type addMeasure struct {
 }
 
 type add_observe struct {
-	Observe int64   `json:"observe"` // unix microsecond timestamp on observing the leader
+	Observe int64       `json:"observe"` // unix microsecond timestamp on observing the leader
 	Queries []add_query `json:"queries"`
 }
 
@@ -77,15 +95,13 @@ func addPerformance_B(cfg config) {
 		log.Printf("found leader %v at endpoint %v\n", leaderId, leaderEp)
 	}
 
-	
-	
 	// add memeber
 	log.Printf("ready to start")
-	
+
 	//start := time.Now()
 
 	list := getAddMemberList(clusterIds)
-	for _, mem := range list{
+	for _, mem := range list {
 		addCli := mustCreateClient(leaderEp)
 		stopCh := make(chan struct{})
 		addDoneCh := make(chan struct{})
@@ -98,7 +114,7 @@ func addPerformance_B(cfg config) {
 		// issue add
 		//issue := time.Now()
 		ctx, _ := context.WithTimeout(context.Background(), time.Minute*5)
-		if _, err := addCli.MemberAdd(ctx,memb, 0); err != nil {
+		if _, err := addCli.MemberAdd(ctx, memb, 0); err != nil {
 			panic(fmt.Sprintf("add failed: %v", err))
 		}
 		close(addDoneCh)
@@ -108,8 +124,6 @@ func addPerformance_B(cfg config) {
 		close(stopCh)
 		addCli.Close()
 	}
-
-
 
 	log.Printf("collect results...")
 }
@@ -152,9 +166,8 @@ func addPerformance(cfg config) {
 
 	stopCh := make(chan struct{})
 
-	
 	addDoneCh := make(chan struct{})
-	
+
 	// add memeber
 	log.Printf("ready to start")
 	addCli := mustCreateClient(leaderEp)
@@ -169,6 +182,119 @@ func addPerformance(cfg config) {
 		panic(fmt.Sprintf("add failed: %v", err))
 	}
 	close(addDoneCh)
+	host := "http://192.168.0.101:2380"
+	user := "ubuntu"
+	pKey := []byte("/home/ubuntu/.ssh/etcd_server")
+
+	var err error
+	var signer ssh.Signer
+
+	signer, err = ssh.ParsePrivateKey(pKey)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	var hostkeyCallback ssh.HostKeyCallback
+	hostkeyCallback, err = knownhosts.New("~/.ssh/known_hosts")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	conf := &ssh.ClientConfig{
+		User:            user,
+		HostKeyCallback: hostkeyCallback,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+	}
+
+	var conn *ssh.Client
+
+	conn, err = ssh.Dial("tcp", host, conf)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer conn.Close()
+
+	var session *ssh.Session
+	var stdin io.WriteCloser
+	var stdout, stderr io.Reader
+
+	session, err = conn.NewSession()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer session.Close()
+
+	stdin, err = session.StdinPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stdout, err = session.StdoutPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stderr, err = session.StderrPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	wr := make(chan []byte, 10)
+
+	go func() {
+		for {
+			select {
+			case d := <-wr:
+				_, err := stdin.Write(d)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for {
+			if tkn := scanner.Scan(); tkn {
+				rcv := scanner.Bytes()
+
+				raw := make([]byte, len(rcv))
+				copy(raw, rcv)
+
+				fmt.Println(string(raw))
+			} else {
+				if scanner.Err() != nil {
+					fmt.Println(scanner.Err())
+				} else {
+					fmt.Println("io.EOF")
+				}
+				return
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	session.Shell()
+
+	for {
+		fmt.Println("$")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+
+		wr <- []byte(text + "\n")
+	}
 
 	// after add
 	log.Print(<-time.After(time.Duration(cfg.After) * time.Second))
@@ -179,7 +305,7 @@ func addPerformance(cfg config) {
 	log.Print(issue)
 }
 func getAddMemberList(clusters [][]uint64) []string {
-	var clrs = []string{"http://192.168.0.101:2380","http://192.168.0.99:2380","http://192.168.0.65:2380","http://192.168.0.181:2380","http://192.168.0.81:2380"}
+	var clrs = []string{"http://192.168.0.101:2380", "http://192.168.0.99:2380", "http://192.168.0.65:2380", "http://192.168.0.181:2380", "http://192.168.0.81:2380"}
 	//var clrs = []string{"http://192.168.0.101:2380"}
 	//var clrs = []string{"http://192.168.0.99:2380"}
 	for _, clr := range clusters {
